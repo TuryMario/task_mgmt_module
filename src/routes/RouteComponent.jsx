f972e02627e70682b7673e6265692a7b46609c06
colinmarklubembe <colinlubembe68@gmail.com>
2023-08-24 15:19:06 +0300

    Made some changes to the TeamList.js

--- src/routes/RouteComponent.jsx
+++ src/routes/RouteComponent.jsx
@@ -4,6 +4,7 @@ import WorkSpace from "../pages/workSpace/WorkSpace";
 // import WorkSpaceDetails from "../pages/workSpaceDetails/WorkSpaceDetails....copy..table";
 // import MainTaskDetailsPage from "../pages/tasks/taskDetails/MainTaskDetailsPage";
 import CreateTeamHandler from "../pages/teams/CreateTeamHandler";
+import TaskListTable from '../components/taskListTable/TaskListTable';
 function RouteComponent() {
   return (
     <div>
@@ -12,6 +13,7 @@ function RouteComponent() {
         {/* <Route path="/workspacedetails" element={<WorkSpaceDetails />} /> */}
         {/* <Route path="/maintaskdetailspage" element={<MainTaskDetailsPage />} /> */}
         <Route path="/create-team-handler" element={<CreateTeamHandler />} />
+        <Route path="/view-tasks" element={<TaskListTable />} />
       </Routes>
     </div>
   );
